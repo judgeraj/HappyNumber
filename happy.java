38952b79e2626d2650d2d7aeb365e77267d54e89
judgeraj <judgeraj42@gmail.com>
2021-01-23 10:37:57 -0800

    WORKING SOLUTION

---
+++ happy.java
@@ -0,0 +1,16 @@
+public boolean isHappy(int n) {
+       HashSet<Integer> seen = new HashSet<>(); 
+        
+       while(n != 1) {
+           int current = n;    
+           int sum = 0;
+           while(current != 0) {
+               sum+=Math.pow(current%10, 2);
+               current/=10;
+           }
+           if(seen.contains(sum)) return false;
+           seen.add(sum);
+           n = sum; 
+       }
+       return true;
+}
